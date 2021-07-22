4ef3e080c16d5a7d97b229ff0f1a5e4a84d93214
AAVolynkin <aavolynkin@gmail.com>
2021-07-22 11:59:27 +0300

    some fix HttpDownloader

--- src/me/alexv/result/HttpDownloader.java
+++ src/me/alexv/result/HttpDownloader.java
@@ -14,8 +14,6 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 /*
     Консольная утилита для скачивания файлов по HTTP протоколу.
@@ -28,23 +26,30 @@ import java.util.concurrent.atomic.AtomicLong;
 public class HttpDownloader implements Runnable {
     private URL url;
     private File file;
+
+    private long downloadBegTime;
+    private long downloadEndTime;
+
+    private int fileSizeInByte;
+
     private IDownloadListener listener;
 
+
     public static void main(String[] args) throws Exception {
-        //Через стримы лишь для сдачи лабы по ним)
+        //Через стримы лишь для сдачи д/з 14.stream))
 
         //считываем входные параметры
-        //1.количество одновременных потоков (если не задано, то 1)
+        //1 параметр: количество одновременных потоков (если не задано, то 1)
         Integer threadsCount = Arrays.stream(args).limit(1).filter(s -> s.matches("^-?\\d+$")).mapToInt(Integer::parseInt).findFirst().orElse(1);
 
-        //3.каталог для хранения скачанных файлов
-        String downloadPath = args[2];
+        //3 параметр: каталог для хранения скачанных файлов
+        File dir = new File(args[2]);
 
-        //2.считываем список ссылок в HashTable, чтобы убрать дубликаты URL,File
+        //2 параметр: считываем список ссылок в HashTable, чтобы убрать дубликаты URL,File
         Map<URL, File> urls = new Hashtable<>();
         Files.lines(Paths.get(args[1])).forEach(s -> {
             try {
-                urls.put(new URL(s.split("\s")[0]), new File(downloadPath + s.split("\s")[1]));
+                urls.put(new URL(s.split("\s")[0]), new File(dir, s.split("\s")[1]));
             } catch (MalformedURLException ignore) {
                 //skip url
             }
@@ -69,28 +74,51 @@ public class HttpDownloader implements Runnable {
 
         //считаем время выполнения
         time = System.currentTimeMillis() - time;
-        System.out.printf("Загружено: %d файлов, %d B\n",listener.getCount(),listener.getSize());
-        System.out.printf("Время: %d миллисекунд\n", time);
-        System.out.printf("Средняя скорость: %d B/s\n" , listener.getCount(),listener.getSize() / time / 1000);
+        System.out.printf("\nЗагружено: %d файлов, %d.%03d MB\n",
+                listener.getCount(),
+                listener.getSize() / (1024 * 1024),
+                listener.getSize() % (1024 * 1024));
+        System.out.printf("Время: %s\n", String.format("%02d:%02d:%02d.%03d",
+                time / 1000 / 3600,
+                time / 1000 / 60 % 60,
+                time / 1000 % 60,
+                time % 1000));
+
+        System.out.printf("Средняя скорость: %f MB/s\n",
+                (double) ((listener.getSize() / (1024 * 1024)) / (time / 1000d)) / listener.getCount());
     }
 
     public HttpDownloader (URL url, File file, IDownloadListener listener) {
         this.url = url;
         this.file = file;
         this.listener = listener;
+        this.downloadBegTime = System.currentTimeMillis();
+    }
+
+    public String getFileName() {
+        return file.getName();
+    }
+
+    public long getDownloadTime() {
+        return this.downloadEndTime - this.downloadBegTime;
+    }
+
+    public int getFileSize() {
+        return fileSizeInByte;
     }
 
     @Override
     public void run() {
-        downloadFile(this.url, this.file, this.listener);
+        downloadFile();
     }
 
-    private void downloadFile(URL url, File file, IDownloadListener listener) {
+    private void downloadFile() {
         try (FileOutputStream fos = new FileOutputStream(file)) {
-            listener.onDownloadBeg(String.format("Загружается файл: %s",file.getName()),url,file);
             long time = System.currentTimeMillis();
 
-            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            listener.onDownloadBeg(this);
+
+            HttpURLConnection urlConn = (HttpURLConnection) this.url.openConnection();
             urlConn.setRequestMethod("GET");
             urlConn.setDoInput(true);
             urlConn.connect();
@@ -100,53 +128,63 @@ public class HttpDownloader implements Runnable {
             byte[] buffer = new byte[128];
             while ((nRead = bis.read(buffer)) != -1) {
                 fos.write(buffer,0, nRead);
+                fileSizeInByte += nRead;
             }
 
-            time = System.currentTimeMillis() - time;
-            listener.onDownloadEnd(String.format("Файл %s загружен: %d B за %d миллисекунд\n",
-                    file.getName(),
-                    file.length(),
-                    time),
-                    url, file);
+            downloadEndTime = System.currentTimeMillis();
+
+            listener.onDownloadEnd(this);
         } catch (Exception ignore) {
-            listener.onError("Ошибка загрузки файла по url " + url.toString());
+            listener.onError(String.format("Ошибка загрузки файла по url %s\n",url.toString()));
         }
     }
 }
 
 //класс для callback, подсчитывает размер файлов и количество успешно загруженных файловё
 class Listener implements IDownloadListener {
-    AtomicInteger count = new AtomicInteger(0);
-    AtomicLong size = new AtomicLong(0);
+    private int count;
+    private int size;
 
     public int getCount() {
-        return count.intValue();
+        return count;
     }
 
-    public long getSize() {
-        return size.longValue();
+    public int getSize() {
+        return size;
     }
 
     @Override
     public void onError(String message) {
-        System.out.println(message);
+        System.out.printf(message);
     }
 
     @Override
-    public void onDownloadBeg(String message, URL url, File file) {
-        System.out.println(message);
+    public void onDownloadBeg(HttpDownloader instance) {
+        System.out.printf("Загружается файл: %s\n",instance.getFileName());
     }
 
     @Override
-    public void onDownloadEnd(String message, URL url, File file) {
-        System.out.println(message);
-        count.incrementAndGet();
-        size.addAndGet(file.length());
+    public void onDownloadEnd(HttpDownloader instance) {
+        //считаем размер и количество скачанных файлов
+        synchronized (this) {
+            size += instance.getFileSize();
+            count++;
+        }
+
+        System.out.printf("Файл %s загружен: %d.%03dMByte за %s\n",
+                instance.getFileName(),
+                instance.getFileSize() / (1024 * 1024),
+                instance.getFileSize() % (1024 * 1024),
+                String.format("%02d:%02d:%02d.%03d",
+                        instance.getDownloadTime() / 1000 / 3600,
+                        instance.getDownloadTime() / 1000 / 60 % 60,
+                        instance.getDownloadTime() / 1000 % 60,
+                        instance.getDownloadTime() % 1000));
     }
 }
 
 interface IDownloadListener{
     void onError(String message);
-    void onDownloadBeg(String message, URL url, File file);
-    void onDownloadEnd(String message, URL url, File file);
+    void onDownloadBeg(HttpDownloader instance);
+    void onDownloadEnd(HttpDownloader instance);
 }
